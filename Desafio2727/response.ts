c9ee12224027cac26a8921383bc797dc8be12851
Daniel San <danielgdt88@hotmail.com>
2024-12-30 16:41:22 -0300

    feat: Desafio 2727

---
+++ Desafio2727/response.ts
@@ -0,0 +1,15 @@
+// Given an object or an array, return if it is empty.
+
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    const objFormated = Object.keys(obj)
+    return objFormated.length === 0 ? true : false
+};
+
