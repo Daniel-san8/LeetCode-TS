a1db73de0371ac1caf0baa9c42fee826c28cc16b
Daniel San <danielgdt88@hotmail.com>
2024-12-29 21:50:26 -0300

    feat: Desafio 2629

---
+++ Desafio2629/response.ts
@@ -0,0 +1,26 @@
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    let iArrFunc = functions.length - 1
+    return function (x) {
+        for (let i = 0; i < functions.length; i++) {
+            x = functions[iArrFunc](x)
+            iArrFunc--
+        }
+        return x
+    }
+};
+
+
+ const fn = compose([x => x + 1, x => 2 * x])
+ fn(4) // 9
+ 
